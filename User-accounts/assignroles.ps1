4e2a46ad4987c8e2f171b17f0fe99974a4209d1d
Erlend-Johan-Dyngeseth <erlend.johan.dyngeseth1@gmail.com>
2021-08-23 21:49:29 +0200

    Revised

--- User-accounts/assignroles.ps1
+++ User-accounts/assignroles.ps1
@@ -7,5 +7,5 @@ New-AzRoleAssignment -SignInName "<emailOrUserprincipalname>" -RoleDefinitionNam
 
 # The following lines show how to assign the roles assigned in this solution (Replace <content> with your desired content)
 New-AzRoleAssignment -SignInName "bob@<suffix>" -RoleDefinitionName "Owner" -Scope "/subscriptions/<subscriptionId>"
-New-AzRoleAssignment -SignInName "dave@<suffix>" -RoleDefinitionName "Owner" -ResourceGroupName "<resourceGroupName>"
+New-AzRoleAssignment -SignInName "dave@<suffix>" -RoleDefinitionName "Contributor" -ResourceGroupName "<resourceGroupName>"
 New-AzRoleAssignment -SignInName "dave@<suffix>" -RoleDefinitionName "Reader" -ResourceGroupName "<resourceGroupName>"
